84ba656abfc9a6ab93f280614e40e40e3a72f9a0
Charles Howard <cdhoward@add123.com>
2016-09-07 17:21:11 -0400

    version step to 15

--- src/main/java/net/torocraft/teletoro/TeleToroMod.java
+++ src/main/java/net/torocraft/teletoro/TeleToroMod.java
@@ -14,7 +14,7 @@ import net.torocraft.teletoro.blocks.BlockEnder;
 public class TeleToroMod {
 
 	public static final String MODID = "teletoro";
-	public static final String VERSION = "1.10.2-14";
+	public static final String VERSION = "1.10.2-15";
 	public static final String MODNAME = "TeleToro";
 
 	@SidedProxy(clientSide = "net.torocraft.teletoro.ClientProxy", serverSide = "net.torocraft.teletoro.ServerProxy")
