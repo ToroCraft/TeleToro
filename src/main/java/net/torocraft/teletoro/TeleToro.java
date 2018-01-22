ccca04e8374aabc5366255bb4a7154ce70038548
Charles Howard <frodare@gmail.com>
2018-01-22 18:40:57 -0500

    upgrade forge

--- src/main/java/net/torocraft/teletoro/TeleToro.java
+++ src/main/java/net/torocraft/teletoro/TeleToro.java
@@ -13,7 +13,7 @@ import net.torocraft.teletoro.blocks.BlockEnder;
 public class TeleToro {
 
 	public static final String MODID = "teletoro";
-	public static final String VERSION = "1.12.1-26";
+	public static final String VERSION = "1.12.2-26";
 	public static final String MODNAME = "TeleToro";
 
 	@SidedProxy(clientSide = "net.torocraft.teletoro.ClientProxy", serverSide = "net.torocraft.teletoro.ServerProxy")
